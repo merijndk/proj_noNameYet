04150c88536c16e1858dae25a75aa478b1cf08d9
Basibo <basjacobs93@gmail.com>
2015-06-07 18:07:17 +0200

    Code herschreven
    :)

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+var tileheight = 94;
+var tilewidth = 80;
+
 function createMap(bounds) {
   var table = [];
-  for (var c = 0; c < bounds; c++) {
+  for (var c = 0; c < bounds*3; c++) {
     var row = [];
     for (var r = 0; r < bounds; r++) {
       row.push(generateTile());
@@ -17,6 +20,22 @@ function generateTile() {
   return {"color": "dark", "oil": Math.floor(Math.random()*10)+1};
 }
 
+function drawMap(map) {
+	for (var i=0; i<map.length; i++){
+		for (var j=0; j<map[i].length; j++){
+			tileinfo = map[i][j];
+			var tile = document.createElement('a');
+			tile.setAttribute('class', 'tile');
+			tile.style.left = String(i%2*60+j*tilewidth*3/2) + "px";
+			tile.style.top = String(i*tileheight/3) + "px";
+			tile.innerHTML = "<img src='images/tile"+tileinfo.color+".png'/>";
+			$('#hexmap').append(tile);
+		}
+	}
+}
+
+var map = createMap(6);
+drawMap(map);
 
 $( "#login" ).click(function() {
   var naam = document.getElementById('naam').value;
@@ -40,4 +59,3 @@ function writeHighscore(naam){
 		}
 	}
 };
-var map = createMap(8);
